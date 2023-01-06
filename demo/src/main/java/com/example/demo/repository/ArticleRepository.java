0ffae2b023bdb6e77dd28d502af8d5ce2795e33f
raeunlee <raeun19@naver.com>
2023-01-06 17:04:53 +0900

    remove

--- demo/src/main/java/com/example/demo/repository/ArticleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.Article;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface ArticleRepository extends JpaRepository<Article, Long> {
-
-}
